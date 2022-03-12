96ffb1a1167d3e7ddbe79c447dc534099537e129
TheNuee <43197392+TheNuee@users.noreply.github.com>
2022-03-13 05:44:31 +0700

    Add files via upload

---
+++ DEYA_TP1_RIZKY_1202210224.py
@@ -0,0 +1,8 @@
+asj = input("Masukkan Jumlah Pengeluaran Bulan Ini : ")
+jsa = input("Masukkan Jumlah Pemasukan Bulan Ini : ")
+
+print("Nama : PT.XYZ")
+print("Alamat : Jl. Yos Sudarso XIII ")
+print("pengeluaran :" + asj)
+print("pemasukan :" + jsa)
+
---
+++ DEYA_TP2_RIZKY_1202210224.py
@@ -0,0 +1,9 @@
+print("=== Hasil Penjualan PT.Pejuang Muda ===")
+
+hol = float(input("Masukkan hasil penjualan besi Hollow bulan ini = "))
+sik = float(input("Masukkan hasil penjualan besi Siku bulan ini = "))
+pla = float(input("Masukkan hasil penjualan besi Plat bulan ini = "))
+bes = float(input("Masukkan hasil penjualan Pipa Besi  bulan ini = "))
+
+hasilrata = (hol+sik+pla+bes)/4
+print("Hasil Rata-Rata Penjualan Bulan Ini = ", hasilrata)
